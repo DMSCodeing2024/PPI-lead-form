276c605dc17355566bff6b8043afac41029f4306
664Parvez <parvezrabbi664@gmail.com>
2024-08-29 17:53:50 +0600

    first1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const apply_btn = document.getElementById("apply_btn")
 
 apply_btn.addEventListener("click", () => {
-    window.location.href = "thankyou.html"
+    window.location.href = "https://dmscodeing2024.github.io/PPI-lead-form/thankyou.html"
 })
